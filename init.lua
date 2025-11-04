084308d3572cc8106b2d0ba42d0d72d0b84ed6ab
Arata <alexisbousquet20@gmail.com>
2025-11-04 20:04:57 +0100

    add norminette

--- init.lua
+++ init.lua
@@ -254,6 +254,38 @@ require("lazy").setup({
     opts = { position = "right" },
   },
 
+  { -- norminette
+    "MoulatiMehdi/42norm.nvim",
+    config = function()
+      local norm = require("42norm")
+
+      norm.setup({
+        header_on_save = true,
+        format_on_save = true,
+        liner_on_change = true,
+      })
+
+      -- Press "F5" key to run the norminette
+      vim.keymap.set("n", "<F5>", function()
+        norm.check_norms()
+      end, { desc = "Update 42norms diagnostics", noremap = true, silent = true })
+      -- Press "Ctrl + f" to format the buffer
+      vim.keymap.set("n", "<leader>f", function()
+        norm.format()
+      end, { desc = "Format buffer on 42norms", noremap = true, silent = true })
+      -- create your commands
+      vim.api.nvim_create_user_command("Norminette", function()
+        norm.check_norms()
+      end, {})
+      vim.api.nvim_create_user_command("Format", function()
+        norm.format()
+      end, {})
+      vim.api.nvim_create_user_command("Stdheader", function()
+        norm.stdheader()
+      end, {})
+    end,
+  },
+
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   "NMAC427/guess-indent.nvim", -- Detect tabstop and shiftwidth automatically
 
